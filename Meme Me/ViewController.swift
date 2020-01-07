6a269f6c405197708d5dbcc2ae4a8327ff538787
clc80 <clc_80@yahoo.com>
2020-01-07 10:52:27 -0800

    Image  Picker Added

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imagePicker: UIBarButtonItem!
+    @IBOutlet var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func imagePickerButtonPressed(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 
+    func imagePickerControllerDidCancel(_: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
+
+
 
